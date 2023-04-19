d12fe4708472b627a5e53368f1f5c8cfdf000e26
jonu400 <122871627+jonu400@users.noreply.github.com>
2023-04-19 14:56:27 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode':200,
-        'body': json.dumps('hello from lambda')
+        'body': json.dumps('hello from lambda.......o1')
     }
